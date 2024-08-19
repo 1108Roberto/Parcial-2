43759ee4975fea566973b44d42dd666a49a55ec1
1108Roberto <116924332+1108Roberto@users.noreply.github.com>
2024-08-18 22:21:35 -0500

    Add files via upload

---
+++ Parcial2.py
@@ -0,0 +1,25 @@
+from flask import Flask, jsonify
+import pandas as pd
+
+app = Flask(__name__)
+
+# Cargar datos
+df = pd.read_csv('')
+
+# Ruta para obtener todos los datos
+@app.route('/api/v1/vaccination', methods=['GET'])
+def get_vaccination_data():
+    data = df.to_dict(orient='records')
+    return jsonify(data)
+
+# Ruta para obtener datos de un año específico
+@app.route('/api/v1/vaccination/<int:year>', methods=['GET'])
+def get_vaccination_data_by_year(year):
+    data = df[df['Year'] == year].to_dict(orient='records')
+    if data:
+        return jsonify(data)
+    else:
+        return jsonify({'error': 'Data not found'}), 404
+
+if __name__ == '__main__':
+    app.run(debug=True)
